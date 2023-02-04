34e7eab1537525e322e9d88dbc3a9bac6d67d028
samim kumar patel <65467139+samimcloud2020@users.noreply.github.com>
2023-02-04 15:45:29 +0530

    Update main.tf

--- xxx/main.tf
+++ xxx/main.tf
@@ -1,9 +1,5 @@
-provider "aws" {
-	region = var.aws_region
-}
-
 module "my_instance_module" {
-        source = "./my_modules/instance"
+        source = "./my-modules/instance"
         ami = "ami-04169656fea786776"
         instance_type = "t2.nano"
         instance_name = "myvm01"
